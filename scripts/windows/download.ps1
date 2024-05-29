0e327b0bfb67f5af268c9f74b1b7298259a32cf6
Jiaqi Luo <6218999+jiaqiluo@users.noreply.github.com>
2024-05-28 17:45:41 -0700

    add GHA workflows

--- scripts/windows/download.ps1
+++ scripts/windows/download.ps1
@@ -13,6 +13,7 @@ if (-not $env:LOCAL_ARTIFACTS) {
     Push-Location artifacts
     curl.exe -fL -O -R https://github.com/rancher/rke2/releases/download/$env:URI_VERSION/rke2.windows-$env:ARCH.tar.gz
     curl.exe -fL -O -R https://github.com/rancher/rke2/releases/download/$env:URI_VERSION/sha256sum-$env:ARCH.txt
+    ls -File
     Pop-Location
 
 }
